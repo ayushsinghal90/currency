31250a7a0559af044dd2f2f9d57191c5b8689c42
Ayush Singhal <ayushsinghal998@gmail.com>
2021-09-11 03:16:43 +0530

    add test cases for query and mutation

--- currency/rates/tests/test_get_rates.py
+++ currency/rates/tests/test_get_rates.py
@@ -1,5 +1,7 @@
+import graphene
 import mock
 
+from currency.schema import Query, Mutation
 from currency.tests.base import BaseTestCase
 from currency.utils.api_response_messages import INPUT_INVALID_MSG
 from currency.utils.commons import read_json
@@ -29,3 +31,44 @@ class TestGetRates(BaseTestCase):
         data = read_json(FIXTURES_PATH.format(file_path='error_get_rates_body.json'))
         response = self._get('/rate/', data)
         self.assertEqual(500, response.status_code)
+
+    @mock.patch('currency.utils.fixer_api.FixerApi.get_latest_rates', FixerApiPatch.get_latest_rates_patch)
+    def test_get_rates_query(self):
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(
+            """{rates(base: "EUR", symbols: ["JPY","USD","INR"]){base,rates {symbol,rate,base}}}""")
+        self.assertIsNone(result.errors)
+
+        response = read_json(FIXTURES_PATH.format(file_path='get_rates_query_response.json'))
+
+        self.assertDictEqual(response, result.data)
+
+    @mock.patch('currency.utils.fixer_api.FixerApi.get_latest_rates', FixerApiPatch.get_latest_rates_failed_patch)
+    def test_get_rates_query_failure(self):
+        schema = graphene.Schema(query=Query)
+        result = schema.execute(
+            """{rates(base: "IND", symbols: ["JPY","USD","INR"]){base,rates {symbol,rate,base}}}""")
+        self.assertIsNotNone(result.errors)
+
+        self.assertDictEqual({'rates': None}, result.data)
+
+    @mock.patch('currency.utils.fixer_api.FixerApi.get_latest_rates', FixerApiPatch.get_latest_rates_patch)
+    def test_update_rates_mutation(self):
+        schema = graphene.Schema(mutation=Mutation)
+        result = schema.execute(
+            """mutation updateRates{updateRates(base: "EUR", symbols: ["JPY","USD","INR"])
+            {ok,currencyRates{base,rates {symbol,rate,base}}}}""")
+        self.assertIsNone(result.errors)
+
+        response = read_json(FIXTURES_PATH.format(file_path='update_rates_mutation_response.json'))
+
+        self.assertDictEqual(response, result.data.get('updateRates'))
+
+    @mock.patch('currency.utils.fixer_api.FixerApi.get_latest_rates', FixerApiPatch.get_latest_rates_failed_patch)
+    def test_update_rates_mutation_failure(self):
+        schema = graphene.Schema(mutation=Mutation)
+        result = schema.execute(
+            """mutation updateRates{updateRates(base: "IND", symbols: ["JPY","USD","INR"])
+            {ok,currencyRates{base,rates {symbol,rate,base}}}}""")
+
+        self.assertDictEqual({'currencyRates': None, 'ok': False}, result.data.get('updateRates'))
