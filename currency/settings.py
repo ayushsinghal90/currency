92ee61370cbc7ec9c3b9c7db79ce6c907dc59cd9
Ayush Singhal <ayushsinghal998@gmail.com>
2021-09-10 23:43:36 +0530

    remove db not required

--- currency/settings.py
+++ currency/settings.py
@@ -88,21 +88,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'currency.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-       'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
