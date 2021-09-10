c1768cfdf4694308da75498ab0b8821d044dcf2f
Ayush Singhal <ayushsinghal998@gmail.com>
2021-09-11 02:05:38 +0530

    send error in get api on failure

--- currency/rates/views.py
+++ currency/rates/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 
 from currency.rates.utils import get_rates
 from currency.utils.commons import get_api_error_response, get_api_success_response
-from currency.utils.api_response_messages import SUCCESS_MESSAGE, ERROR_MESSAGE, SUCCESS_CODE, INPUT_INVALID
+from currency.utils.api_response_messages import SUCCESS_MESSAGE, ERROR_MESSAGE, SUCCESS_CODE, INPUT_INVALID, SERVER_ERROR
 
 LOG = logging.getLogger(__name__)
 
@@ -19,8 +19,11 @@ class RatesView(APIView):
                 return get_api_error_response(status=401, message=ERROR_MESSAGE[INPUT_INVALID])
 
             rates = get_rates(base, symbols)
-            return get_api_success_response(data={"base": base, "rates": rates}, message=SUCCESS_MESSAGE[SUCCESS_CODE], status=201)
+            return get_api_success_response(data={"base": base, "rates": rates},
+                                            message=SUCCESS_MESSAGE[SUCCESS_CODE],
+                                            status=201)
 
         except Exception as e:
             LOG.error("Key addition failed {}".format(e), exc_info=True)
-            return get_api_error_response(status=500, message=SUCCESS_MESSAGE[SUCCESS_CODE])
+            return get_api_error_response(status=500,
+                                          message=ERROR_MESSAGE[SERVER_ERROR])
